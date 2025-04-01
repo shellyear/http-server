9310bd151e0e6210b8186cf4b3c7dcc67ffc3921
shellyear <boolevafunkce@gmail.com>
2025-04-01 22:09:41 +0500

    [chore]

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import net from "net";
 
 // The server can be a TCP server or an IPC server, depending on what it listen() to
 const tcpServer = net.createServer((socket: net.Socket) => {
-  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n")); // sending TCP response, 
-  // it's still not exactly HTTP response; While the data follows the HTTP response format (HTTP/1.1 200 OK\r\n\r\n), it does not behave like a full HTTP server.
+  /* 
+    sending TCP response,
+    it's still not exactly HTTP response; While the data follows the HTTP response format (HTTP/1.1 200 OK\r\n\r\n), it does not behave like a full HTTP server.
+    A real HTTP server should handle request parsing, different HTTP methods (GET, POST), headers, and more. 
+  */
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
   socket.on("close", () => {
     socket.end();
   });
