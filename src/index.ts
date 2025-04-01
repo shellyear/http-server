9bfcf2b33bffd0064a295cc8b388faa4e7988c6b
shellyear <boolevafunkce@gmail.com>
2025-04-01 21:38:36 +0500

    [any message]

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import net from "net";
 
 // The server can be a TCP server or an IPC server, depending on what it listen() to
 const tcpServer = net.createServer((socket: net.Socket) => {
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
   socket.on("close", () => {
     socket.end();
   });
